0cdef0f3f55af056f980a1b2d74f985b675707dc
Tj Hall <tjhall047@gmail.com>
2024-12-09 13:37:36 -0500

    subtracting calculated value only returning whats left

--- system.js
+++ system.js
@@ -30,8 +30,11 @@ function bytesToGb(memInBytes) {
 
 function splitUptime(uptime) {
     const days = uptime / 86400;
+    uptime -= 86400 * days; // Subtracting time from time to calculate to account for whats already been calculated
     const hrs = uptime / 3600;
+    uptime -= 3600 * hrs;
     const mins = uptime / 60;
+    uptime -= 60 * mins;
     const uptimeSplitArray = [Math.floor(days), Math.floor(hrs), Math.floor(mins), Math.floor(uptime)]; // Days, Hrs, Mins, Seconds
 
     return uptimeSplitArray;
