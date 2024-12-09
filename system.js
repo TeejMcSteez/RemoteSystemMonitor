dbe041df3b62f7f21a5a74956c8edece65669706
Tj Hall <tjhall047@gmail.com>
2024-12-09 13:41:37 -0500

    needed to floor values before they entered the array

--- system.js
+++ system.js
@@ -29,13 +29,13 @@ function bytesToGb(memInBytes) {
 }
 
 function splitUptime(uptime) {
-    const days = uptime / 86400;
+    const days = Math.floor(uptime / 86400);
     uptime -= 86400 * days; // Subtracting time from time to calculate to account for whats already been calculated
-    const hrs = uptime / 3600;
+    const hrs = Math.floor(uptime / 3600);
     uptime -= 3600 * hrs;
-    const mins = uptime / 60;
+    const mins = Math.floor(uptime / 60);
     uptime -= 60 * mins;
-    const uptimeSplitArray = [Math.floor(days), Math.floor(hrs), Math.floor(mins), Math.floor(uptime)]; // Days, Hrs, Mins, Seconds
+    const uptimeSplitArray = [days, hrs, mins, uptime]; // Days, Hrs, Mins, Seconds
 
     return uptimeSplitArray;
 }
