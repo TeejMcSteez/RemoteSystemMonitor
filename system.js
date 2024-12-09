bbaae26bb42b1223392ca939a1fb678fe7c3af61
Tj Hall <tjhall047@gmail.com>
2024-12-08 23:48:13 -0500

    Changed some styles for reading ease and converted memory to GB not bytes

--- system.js
+++ system.js
@@ -3,19 +3,29 @@ const os = require("node:os");
 function getCurrentMemory() {
     const freeMem = os.freemem();
 
-    return freeMem;    
+    const freeMemInGb = bytesToGb(freeMem);
+
+    return freeMemInGb;    
 }
 
 function getTotalMemory() {
     const totalMem = os.totalmem();
 
-    return totalMem;
+    const totalMemInGb = bytesToGb(totalMem);
+
+    return totalMemInGb;
 }
 
 function getUptime() {
     const uptime = os.uptime();
 
     return uptime;
+}
+
+function bytesToGb(memInBytes) {
+    const memInGb = memInBytes / 1e9; // Dividing the bytes by a billion
+
+    return memInGb;
 }
 
 module.exports = {getCurrentMemory, getUptime, getTotalMemory};
