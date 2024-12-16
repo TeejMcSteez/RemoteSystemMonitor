2211952ffb8a1df54d2227ffc2aa462e4c95b272
TeejMcSteez <tjhall047@gmail.com>
2024-12-16 13:43:06 -0500

    Edited the README with examples and notes as well as updated the build script to test for any NCT sensor and more verbose output

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-Local HTTP system monitor for linux systems using hwmon files and node alongside some built in node monitoring functions. 
-One can download source code and set up thereself or download the build script which will pre-install everything for you once ran in the directory using bash.
+# Hardware Monitor for Linux Systems Using Hwmon
+![Example Image](Github/image.png)
+Local HTTP system monitor for linux systems using hwmon files and node alongside some built in node monitoring functions.
+--- 
+One can download source code and set up thereself or download the build script which will pre-install everything for you once ran in the install directory using bash.
+---
+**NOTE** this does not work on Virtual Machines as the sensors-detect cannot detect sensors on the real machine
--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
-
+# hwmon
+hwmon="/sys/class/hwmon"
+env_file=".env"
 # Clears or creates .env file
 > .env 
 
@@ -7,22 +9,17 @@ echo "Starting build . . ."
 
 echo "Please enter the hostname for the HTTP server . . ."
 read HOSTNAME
+echo "Please enter the port for the server to listen on . . ."
+read PORT
 
 echo "Building node server . . ."
 
 npm init -y
+echo "NPM initialized, Installing express"
 npm i express
+echo "Installed express successfully . . . Installing dotenv"
 npm i dotenv
-
 echo "Initialized npm and installed necessary packages"
-
-# Network connection
-
-PORT=3000
-
-# hwmon
-hwmon="/sys/class/hwmon"
-env_file=".env"
 
 echo "Starting file detection . . ."
 
@@ -39,24 +36,17 @@ for dir in "$hwmon"/*; do
         echo "CPU_TEMPERATURE_DIRECTORY=$dir" >> "$env_file"
         echo "Appended $content to $env_file"
     fi
-    if [[ "$content" == "nct6793" ]]; then # My specific PC all round sensor need to do research on all possible values used by modern PC's
+    if [[ "$content" =~ nct[0-9]+ ]]; then # My specific PC all round sensor need to do research on all possible values used by modern PC's
         echo "MOTHERBOARD_DIRECTORY=$dir" >> "$env_file"
         echo "Appended $content to $env_file"
     fi
-
-    # Replacing conditionals with grep
-    
 done
 
 echo "Adding hostname and port to env variables"
 echo "HOSTNAME=$HOSTNAME" >> "$env_file"
 echo "PORT=$PORT" >> "$env_file"
 
-echo "Starting git clone"
-
-mkdir -p "src"
-
-cd "src"
+echo "Starting git pull"
 
 git init 
 git remote add origin https://github.com/TeejMcSteez/RemoteSystemMonitor
@@ -64,4 +54,12 @@ git pull origin master
 
 cd ..
 
-echo "Build Successful"
+echo "Repo pulled successfully, build Successful would you like to start the server? (y/n)"
+read choice 
+if [["$choice" == "y"]]; then
+    echo "Starting node . . ."
+    node "src/index.js"
+fi
+if [["$choice" == "n"]]; then 
+    echo "Exiting build script ready for deployement"
+fi
